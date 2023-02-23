6892771eb412723c1c6234f2516eab6b3a8d6a62
ludmilpaulo <ludmilpaulo@gmail.com>
2023-02-23 12:27:19 +0200

    redux

---
+++ redux/slices/basketSlice.tsx
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getAllCartFoods, getTotalCartItemPrice } from "utils/helpers";
+
+interface Meals {
+    foods : any;
+    meals: any;
+    food: any;
+    resImage: string;
+    resName: string;
+    resId: number;
+    category: string;
+    id: number;
+    image: string;
+    name: string;
+    price: number;
+    quantity: number;
+    short_description: string;
+  }
+
+const initialState = {
+  items: [],
+};
+
+const basketSlice = createSlice({
+  name: "busket",
+  initialState,
+  reducers: {
+    updateBusket: (state, action) => {
+      state.items = action.payload;
+    },
+  },
+});
+
+export const { updateBusket } = basketSlice.actions;
+
+export const selectCartItems = (state: { busket: { items: any } }) =>
+  state.busket.items;
+export const selectTotalPrice = (state: { busket: { items: any } }) =>
+  getTotalCartItemPrice(state.busket.items);
+export const selectTotalItems = (state: { busket: { items: any } }) =>
+  getAllCartFoods(state.busket.items);
+
+export default basketSlice.reducer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        bg_image: "url('../assets/bg.png')",
+      },
+      backgroundSize: {
+        auto: "auto",
+        cover: "cover",
+        contain: "contain",
+        "50%": "50%",
+        16: "4rem",
+      },
+    },
   },
   plugins: [],
-}
+};

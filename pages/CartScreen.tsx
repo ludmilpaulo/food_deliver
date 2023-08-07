d59e5e1717cc703789350395166e5b927733aac7
ludmilpaulo <ludmilpaulo@gmail.com>
2023-08-07 21:46:11 +0200

    payment

--- pages/CartScreen.tsx
+++ pages/CartScreen.tsx
@@ -60,6 +60,34 @@ const CartSreen = () => {
                 </p>
               </div>
               <CartMenu />
+
+              <div className="flex justify-center md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
+                  <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6   ">
+                      <h3 className="text-xl font-semibold leading-5 text-gray-800">
+                        Summary
+                      </h3>
+                      <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
+                        <div className="flex justify-between  w-full">
+                          <p className="text-base leading-4 text-gray-800">
+                            Subtotal
+                          </p>
+                          <p className="text-base leading-4 text-gray-600">
+                            {totalPrice}Kz
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex justify-between items-center w-full">
+                        <p className="text-base font-semibold leading-4 text-gray-800">
+                          Total
+                        </p>
+                        <p className="text-base font-semibold leading-4 text-gray-600">
+                          {totalPrice}Kz
+                        </p>
+                      </div>
+                  </div>
+
+              </div>
+
               <div className="mt-8 flex flex-col justify-start items-start w-full space-y-8 ">
                 <input
                   value={userAddress}

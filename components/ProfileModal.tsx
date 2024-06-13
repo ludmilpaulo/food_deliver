7ea2fc22d90dc2e3e7da191d1705096ce6f91016
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2024-06-13 11:56:41 +0200

    education

--- components/ProfileModal.tsx
+++ components/ProfileModal.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { logoutUser, selectUser } from "../redux/slices/authSlice";
 import withAuth from "@/components/ProtectedPage";
 import { baseAPI } from "@/services/types";
+import { Transition } from '@headlessui/react';
 
 type ImageInfo = {
   uri: string;
@@ -29,13 +30,14 @@ const ProfileModal: React.FC<ProfileModalProps> = ({ isOpen, onClose, userDetail
   const [firstName, setFirstName] = useState<string>(userDetails?.first_name || '');
   const [lastName, setLastName] = useState<string>(userDetails?.last_name || '');
   const [phone, setPhone] = useState<string>(userDetails?.phone || '');
+  const [loading, setLoading] = useState<boolean>(false);
   const userToken = user.token;
   const router = useRouter();
 
   useEffect(() => {
     const userLocation = async () => {
       if (typeof navigator === "undefined" || !navigator.geolocation) {
-        alert("Geolocation is not supported by your browser.");
+        alert("Geolocalização não é suportada pelo seu navegador.");
         return;
       }
 
@@ -56,7 +58,7 @@ const ProfileModal: React.FC<ProfileModalProps> = ({ isOpen, onClose, userDetail
           }
         },
         (error) => {
-          alert("Permission to access location was denied");
+          alert("Permissão para acessar localização foi negada");
         }
       );
     };
@@ -72,9 +74,11 @@ const ProfileModal: React.FC<ProfileModalProps> = ({ isOpen, onClose, userDetail
 
   const userUpdate = async () => {
     if (!imageInfo) {
-      alert("Please select an image first");
+      alert("Por favor, selecione uma imagem primeiro");
       return;
     }
+
+    setLoading(true);
 
     const formData = new FormData();
     formData.append('avatar', imageInfo);
@@ -112,81 +116,99 @@ const ProfileModal: React.FC<ProfileModalProps> = ({ isOpen, onClose, userDetail
         console.error(resp);
       }
     } catch (error) {
-      console.error("Error:", error);
-      alert("An error occurred. Please try again.");
+      console.error("Erro:", error);
+      alert("Ocorreu um erro. Por favor, tente novamente.");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div className={`fixed inset-0 z-50 flex items-center justify-center ${isOpen ? '' : 'hidden'}`}>
-      <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
-      <div className="bg-white rounded-lg p-6 relative z-10 w-full max-w-md mx-auto">
-        <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" onClick={onClose}>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-        <div className="flex flex-col items-center space-y-5">
-          <div className="rounded-full overflow-hidden w-48 h-48 mt-4">
-            {imageInfo && (
-              <Image
-                src={URL.createObjectURL(imageInfo)}
-                alt="User Image"
-                width={192}
-                height={192}
-                layout="responsive"
-                unoptimized={true}
+    <>
+      <Transition
+        show={loading}
+        enter="transition-opacity duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-300"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
+          <div className="w-16 h-16 border-t-4 border-b-4 border-blue-500 rounded-full animate-spin"></div>
+        </div>
+      </Transition>
+      <div className={`fixed inset-0 z-50 flex items-center justify-center ${isOpen ? '' : 'hidden'}`}>
+        <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
+        <div className="bg-white rounded-lg p-6 relative z-10 w-full max-w-md mx-auto">
+          <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" onClick={onClose}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+          <div className="flex flex-col items-center space-y-5">
+            <div className="rounded-full overflow-hidden w-48 h-48 mt-4">
+              {imageInfo && (
+                <Image
+                  src={URL.createObjectURL(imageInfo)}
+                  alt="User Image"
+                  width={192}
+                  height={192}
+                  layout="responsive"
+                  unoptimized={true}
+                />
+              )}
+            </div>
+            <div>
+              <input
+                type="file"
+                accept="image/*"
+                capture
+                onChange={handleTakePhotoOrSelect}
+                aria-label="Escolhe a tua foto de perfil"
               />
-            )}
-          </div>
-          <div>
-            <input
-              type="file"
-              accept="image/*"
-              capture
-              onChange={handleTakePhotoOrSelect}
-            />
-          </div>
-          <div className="w-full">
-            <input
-              className="border w-full p-4 rounded"
-              placeholder="Primeiro Nome"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </div>
-          <div className="w-full">
-            <input
-              className="border w-full mt-5 p-4 rounded"
-              placeholder="Ultimo Nome"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </div>
-          <div className="w-full">
-            <input
-              className="border w-full mt-5 p-4 rounded"
-              placeholder="Número de Telefone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </div>
-          <div className="w-full">
-            <input
-              className="border w-full mt-5 p-4 rounded"
-              placeholder="Endereço"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            />
-          </div>
-          <div>
-            <button className="bg-blue-500 text-white p-4 rounded mt-5" onClick={userUpdate}>
-              Atualize seu Perfil
-            </button>
+            </div>
+            <div className="w-full">
+              <input
+                className="border w-full p-4 rounded"
+                placeholder="Primeiro Nome"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+            <div className="w-full">
+              <input
+                className="border w-full mt-5 p-4 rounded"
+                placeholder="Último Nome"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+            <div className="w-full">
+              <input
+                className="border w-full mt-5 p-4 rounded"
+                placeholder="Número de Telefone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <div className="w-full">
+              <input
+                className="border w-full mt-5 p-4 rounded"
+                placeholder="Endereço"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+            <div>
+              <button className="bg-blue-500 text-white p-4 rounded mt-5" onClick={userUpdate}>
+                Atualize seu Perfil
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
